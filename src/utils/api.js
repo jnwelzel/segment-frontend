c23106c69e11bad8bee059e70b7109f99b9f8c19
Jon Welzel <jnwelzel@gmail.com>
2017-08-28 16:49:46 -0300

    env var fix

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,8 +4,8 @@ const LOCAL_API_URL = 'http://localhost:8080'
 
 const getBaseUrl = () => {
   if (process.env.NODE_ENV !== undefined && process.env.NODE_ENV === 'production') {
-    if (process.env.PRODUCTION_API_URL === undefined) {
-      throw new Error('"PRODUCTION_API_URL environment variable not found"')
+    if (process.env.REACT_APP_PRODUCTION_API_URL === undefined) {
+      throw new Error('"REACT_APP_PRODUCTION_API_URL environment variable not found"')
     }
     
     return process.env.PRODUCTION_API_URL
